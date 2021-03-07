d3d2537e983753d3782f36d657f0e582c8fdb8e6
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-07 04:12:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,14 @@
 def fizzbuzz(num)
   if num % 3 == 0
     "Fizz"
+  end
   elsif num % 5 == 0
     "Buzz"
+  end
   elsif num % 3 == 0 && num % 5 == 0
     "FizzBuzz"
+  end
   else
     nil
+  end
 end
